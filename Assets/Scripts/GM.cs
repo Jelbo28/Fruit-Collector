e970991016c10f90678c69c005bf6b534ff1650c
Aram Joseph <ajoseph@utc4me.org>
2016-03-25 08:47:31 -0400

    Fixed Timer and Changed Level Layoout

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -4,17 +4,19 @@ using UnityEngine.UI;
 
 public class GM : MonoBehaviour 
 {
-	[SerializeField]
+    #region variables
+    [SerializeField]
 	int time = 0;
 	[SerializeField]
 	int score = 0;
+    [SerializeField]
+    float timeLeft;
 
-	[SerializeField]
+
+    [SerializeField]
 	Text timeText;
 	[SerializeField]
 	Text scoreText;
-	[SerializeField]
-	GameObject gameOver;
 	[SerializeField]
 	GameObject player;
 
@@ -22,33 +24,40 @@ public class GM : MonoBehaviour
 	static GM instance = null;
 
 	AudioSource appleCollect;
+    #endregion
 
-	void Awake ()
+    void Awake ()
 	{
 		timeText.text = "Time: 120";
 		scoreText.text = "Score: 0";
-		gameOver.SetActive (false);
+        /*
 		AudioSource[] audios = GetComponents<AudioSource> ();
 		appleCollect = audios [0];
+        */
 
 		if (instance == null)
 			instance = this;
 		else if (instance != this)
 			Destroy (gameObject);
-
 	}
 
-	void CheckGameover()
+    void Update()
+    {
+        Timer();
+    }
+
+    void Timer()
+    {
+        timeLeft -= Time.deltaTime;
+        //Debug.Log(timeLeft);
+        timeText.text = ("Time: " + Mathf.FloorToInt(timeLeft));
+        if (timeLeft <= 0)
+        {
+            GameOver();
+        }
+    }
+	void GameOver()
 	{
-		if (time <= 0)
-		{
 
-		}
 	}
-
-
-
-
-
-
 }
